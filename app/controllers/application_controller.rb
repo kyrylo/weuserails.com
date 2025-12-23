083ca3a6554697332de1c26936ead2126264805c
Kyrylo Silin <silin@kyrylo.org>
2025-12-23 20:17:04 +0200

    Revert "feat: add user context to lapsoss error tracking"
    This reverts commit 70fcef5dd3bb3405251a11f197be94a2599ced57.
    
    I get this error in development:
    
    ```
    NoMethodError (undefined method 'action_name' for an instance of String):
    
    app/controllers/web_apps_controller.rb:10:in 'WebAppsController#index'
    ```

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,6 @@
 class ApplicationController < ActionController::Base
   include Authentication, Breadcrumbs
-  include Lapsoss::RailsControllerContext
-
-  before_action :set_lapsoss_user_context
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
-
-  private
-
-  def set_lapsoss_user_context
-    return unless Current.user
-
-    Lapsoss.current_scope.set_user(id: Current.user.id)
-  end
 end
