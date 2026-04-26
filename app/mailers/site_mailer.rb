3eaa3af5d167c332117c172bd4da1cfa9ff6928b
Kyrylo Silin <silin@kyrylo.org>
2026-04-26 11:47:39 +0800

    fixup! stop posting to X

--- app/mailers/site_mailer.rb
+++ app/mailers/site_mailer.rb
@@ -5,12 +5,12 @@ class SiteMailer < ApplicationMailer
   def published_email
     @site = params[:site]
     @user = @site.user
-    mail(to: @user.email_address, subject: "#{@site.title} now featured on We Use Rails")
+    mail(to: @user.email_address, subject: "#{@site.title} now featured on We Use Rails", from: ENV.fetch("SMTP_USERNAME"))
   end
 
   def rails_confirmation_email
     @site = params[:site]
     @user = @site.user
-    mail(to: @user.email_address, subject: "#{@site.title} confirmation of Ruby on Rails usage")
+    mail(to: @user.email_address, subject: "#{@site.title} confirmation of Ruby on Rails usage", from: ENV.fetch("SMTP_USERNAME"))
   end
 end
