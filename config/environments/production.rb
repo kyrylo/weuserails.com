92c023a035c469008d638bd1b29d888cfc12c3f2
Kyrylo Silin <silin@kyrylo.org>
2026-04-26 09:58:27 +0800

    Revert "try to restore mission control access"
    This reverts commit 3527ec27e8a618d03389b1ff085b6c57e8544370.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,4 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-
-  config.mission_control.jobs.http_basic_auth_enabled = false
 end
